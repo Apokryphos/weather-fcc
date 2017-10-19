cbddf63756e3de1c3ddeeb4c459cb810b7f9698f
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-10-19 16:30:13 -0400

    Temperature component can now toggle between Fahrenheit and Celsius scales. Added Classnames package.

--- src/js/app/components/temperature.jsx
+++ src/js/app/components/temperature.jsx
@@ -1,12 +1,45 @@
 const React = require('react');
+const classNames = require('classnames');
 
 class Temperature extends React.Component {
   constructor(props) {
     super(props);
 
+    //  Temperature is stored as celsius and converted when needed
     this.state = {
       celsius: false
     };
+  }
+
+  renderUom() {
+    const setCelsius = enabled => {
+      this.setState({
+        celsius: enabled
+      });
+    };
+
+    let fahrenheit = this.state.celsius ? () => setCelsius(false) : null;
+    let celsius = this.state.celsius ? null : () => setCelsius(true);
+
+    let fahrenheitClass = classNames({
+      'temperature-uom': this.state.celsius
+    });
+
+    let celsiusClass = classNames({
+      'temperature-uom': !this.state.celsius
+    });
+
+    return (
+      <sup>
+        <span className={fahrenheitClass} onClick={fahrenheit}>
+          &#176;F
+        </span>
+        |
+        <span className={celsiusClass} onClick={celsius}>
+          &#176;C
+        </span>
+      </sup>
+    );
   }
 
   render() {
@@ -15,7 +48,12 @@ class Temperature extends React.Component {
       temp = temp * 1.8 + 32;
     }
 
-    return <div className="temperature">{Math.floor(temp)}</div>;
+    return (
+      <div className="temperature">
+        {Math.floor(temp)}
+        {this.renderUom()}
+      </div>
+    );
   }
 }
 
