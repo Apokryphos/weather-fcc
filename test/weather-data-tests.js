fb761d4c26bafc00bc67efa623287d1a57a0b4d7
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-10-19 21:43:00 -0400

    Added another weather API test dataset and test.

--- test/weather-data-tests.js
+++ test/weather-data-tests.js
@@ -3,11 +3,20 @@ const test = require('tape');
 const WeatherData = require('./../src/js/weather/weather-data.js');
 const WeatherStatus = require('./../src/js/weather/weather-status.js');
 
-test('Shuzenji, JP', t => {
+test('Rain - Shuzenji, JP', t => {
   const json = require('./weather-api-data/01.json');
   const data = new WeatherData(json);
   t.equal(data.location, 'Shuzenji, JP');
   t.equal(data.temperature, 12);
   t.equal(data.status.some(s => s === WeatherStatus.RAIN), true);
+  t.end();
+});
+
+test('Clouds - Shuzenji, JP', t => {
+  const json = require('./weather-api-data/02.json');
+  const data = new WeatherData(json);
+  t.equal(data.location, 'Shuzenji, JP');
+  t.equal(data.temperature, 28.23);
+  t.equal(data.status.some(s => s === WeatherStatus.CLOUDY), true);
   t.end();
 });
